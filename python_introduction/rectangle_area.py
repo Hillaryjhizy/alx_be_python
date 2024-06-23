1b7c70ccb43be3546420516c06db3d4e5b09a6b6
Joseph Hillary Enyinnaya <156797451+Hillaryjhizy@users.noreply.github.com>
2024-06-23 07:06:01 +0100

    Add files via upload

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,7 @@
+#Python script that calculates the area of a rectangle
+#variable declaration for parameters
+length = 10
+width = 5
+#Calculating the area of the rectangle
+area = length * width
+print(area)
