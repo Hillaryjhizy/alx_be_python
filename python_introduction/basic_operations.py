db3202af42345be19fda62f5bcae260dd956f60b
Joseph Hillary Enyinnaya <156797451+Hillaryjhizy@users.noreply.github.com>
2024-06-23 06:16:57 +0100

    Add files via upload

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#practice basic arithmetic operations in Python by performing predefined calculations.
+#Given Variable identifier.
+number1 = 10 
+number2 = 5
+#Executing Addition Function
+Addition = number1 + number2
+print(Addition)
+
+#Executing Subtraction Function
+Subtraction = number1 - number2
+print(Subtraction)
+
+#Executing Multiplication Function
+Multiplication = number1 * number2
+print(Multiplication)
